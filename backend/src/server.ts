2e5af3b405b20c077022a1bc7434db7c37d4ae3f
David Martschenko <david.martschenko@web.de>
2020-12-21 00:57:53 +0100

    Add simple server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,15 @@
-console.log("test");
+import express from "express";
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server started: http://localhost:8080");
+});
+app.use(express.json());
+
+app.get("/test", (_, res) => {
+  res.send({ test: "success" });
+});
+
+// use only in production
+// app.use(express.static(path.join(__dirname, "../../frontend/build")));
