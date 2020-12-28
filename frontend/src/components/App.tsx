a4a69ec2dd0e173bdb8317fa6f45866e6ef65346
David Martschenko <david.martschenko@web.de>
2020-12-28 19:15:06 +0100

    add Header to App.tsx

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,22 +3,36 @@ import store from "../features/store";
 import { Provider } from "react-redux";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { routes, routesArray } from "../routes";
+import { Header } from "./Header";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#005B52",
+    },
+  },
+});
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          {routesArray.map((r) => (
-            <Route path={r.path} key={r.path} {...r.routeProps}>
-              {<r.component {...r.props} />}
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            {routesArray.map((r) => (
+              <Route path={r.path} key={r.path} {...r.routeProps}>
+                {<r.component {...r.props} />}
+              </Route>
+            ))}
+            <Route>
+              <Redirect to={routes.home.path} />
             </Route>
-          ))}
-          <Route>
-            <Redirect to={routes.home.path} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 };
