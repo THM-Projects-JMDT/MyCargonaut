7adc328aa468c4e5d3db1592e40d1681e190ec5d
David Martschenko <david.martschenko@web.de>
2020-12-29 14:53:48 +0100

    add Header test

---
+++ frontend/src/components/Header/Header.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Header } from "./index";
+import { BrowserRouter } from "react-router-dom";
+
+const header = (
+  <BrowserRouter>
+    <Header />
+  </BrowserRouter>
+);
+
+it("contains 'MyCargonaut'", () => {
+  const { getByText } = render(header);
+  expect(getByText("My")).toBeInTheDocument();
+  expect(getByText("Cargonaut")).toBeInTheDocument();
+});
+
+it("contains the logo with a link to the start page", () => {
+  const { getByTestId } = render(header);
+  expect(getByTestId("logo").closest("a")).toHaveAttribute("href", "/");
+});
+
+// TODO: add tests when routing is implemented
