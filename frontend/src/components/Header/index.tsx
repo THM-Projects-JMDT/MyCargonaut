d5a08596e6b782928069f3bc879b8973adfd3667
David Martschenko <david.martschenko@web.de>
2020-12-29 17:00:01 +0100

    move logo to assets folder

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo_car.png";
+import logo from "../../assets/logo_car.png";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { useStyles } from "./style";
 import { NavElements } from "./NavElements";
