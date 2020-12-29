373c29230987c7790076b8d93114d0ff8122e5a7
David Martschenko <david.martschenko@web.de>
2020-12-29 17:24:31 +0100

    use svg logo

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "../../assets/logo_car.png";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import logo from "../../assets/logo_car.svg";
+import { AppBar, Toolbar } from "@material-ui/core";
 import { useStyles } from "./style";
 import { NavElements } from "./NavElements";
 import { Link } from "react-router-dom";
@@ -12,18 +12,8 @@ export const Header: React.FC = () => {
       <AppBar className={classes.appBar} position="static">
         <Toolbar>
           <Link to="/">
-            <img
-              data-testid={"logo"}
-              src={logo}
-              alt="logo"
-              style={{ marginBottom: 10, marginRight: 5 }}
-            />
+            <img data-testid={"logo"} src={logo} alt="logo" />
           </Link>
-          <Typography variant="h4" color="primary">
-            <i>
-              <b>My</b>Cargonaut
-            </i>
-          </Typography>
           <NavElements />
         </Toolbar>
       </AppBar>
