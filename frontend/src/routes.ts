46ef5230f6e53a1e79a81b8f2d7e5bd73a3494a7
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-12-30 13:48:33 +0100

    added start, login, register route

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,5 +1,8 @@
 import { RouteProps } from "react-router-dom";
 import { Dashboard } from "./components/Dashboard";
+import { Login } from "./components/Login";
+import { Registration } from "./components/Registration";
+import { StartPage } from "./components/StartPage";
 
 export interface Route {
   path: string;
@@ -13,10 +16,23 @@ export interface Routes {
 }
 
 export const routes: Routes = {
-  home: {
+  start: {
     path: "/",
     routeProps: { exact: true },
+    component: StartPage,
+  },
+  home: {
+    path: "/home",
+    routeProps: { exact: true },
     component: Dashboard,
+  },
+  login: {
+    path: "/login",
+    component: Login,
+  },
+  createAccount: {
+    path: "/account/create",
+    component: Registration,
   },
 };
 
