52d3b9eac13c0c439bb3e05c00762ffbb1af63a2
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2020-12-30 13:49:20 +0100

    rename Header.style

---
+++ frontend/src/components/Header/Header.style.tsx
@@ -0,0 +1,28 @@
+import { makeStyles } from "@material-ui/core";
+
+const headerHeight = "52px";
+
+export const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "white",
+    height: headerHeight,
+    zIndex: 2,
+  },
+  toolbar: {
+    minHeight: headerHeight,
+  },
+  buttonsGroup: {
+    marginLeft: "auto",
+  },
+  button: {
+    width: theme.spacing(17),
+    marginRight: theme.spacing(2),
+  },
+  moneyIcon: {
+    color: "#FFE600",
+    marginRight: theme.spacing(1),
+  },
+  logo: {
+    height: headerHeight,
+  },
+}));
