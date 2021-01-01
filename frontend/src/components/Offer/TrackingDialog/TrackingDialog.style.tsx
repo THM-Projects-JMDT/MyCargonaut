9ae2f464a562e26d52047d88fe4824753cf224c1
David Martschenko <david.martschenko@web.de>
2021-01-01 18:56:53 +0100

    add TrackingDialog component

---
+++ frontend/src/components/Offer/TrackingDialog/TrackingDialog.style.tsx
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  date: {
+    color: "#707070",
+  },
+  message: {
+    borderRadius: theme.spacing(1),
+    padding: theme.spacing(1),
+    backgroundColor: "#c0c0c0",
+  },
+}));
---
+++ frontend/src/components/Offer/TrackingDialog/index.tsx
@@ -0,0 +1,93 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import { useStyles } from "./TrackingDialog.style";
+
+export interface TrackingDetails {
+  state: string; // TODO: use 'TrackingState" type
+  lastMessage?: string;
+  lastMessageDate?: Date;
+}
+
+export interface TrackingDialogProps {
+  tracking: TrackingDetails;
+  open: boolean;
+  onClose: () => void;
+}
+
+export const getStepByState = (trackingState: string) => {
+  switch (trackingState) {
+    case "waiting":
+      return 0;
+    case "inProgress":
+      return 1;
+    case "delivered":
+      return 2;
+  }
+};
+
+export const TrackingDialog: React.FC<TrackingDialogProps> = ({
+  tracking,
+  open,
+  onClose,
+}) => {
+  const classes = useStyles();
+  const activeStep = getStepByState(tracking.state);
+
+  const handleOutsideClick = (event: any) => {
+    event.stopPropagation();
+    onClose();
+  };
+
+  return (
+    <div onClick={handleOutsideClick}>
+      <Dialog open={open} fullWidth>
+        <DialogTitle>Tracking-Status</DialogTitle>
+        <DialogContent>
+          <Stepper alternativeLabel activeStep={activeStep}>
+            <Step>
+              <StepLabel>{activeStep === 0 ? "Bereit!" : ""}</StepLabel>
+            </Step>
+            <Step>
+              <StepLabel>{activeStep === 1 ? "Auf dem Weg!" : ""}</StepLabel>
+            </Step>
+            <Step>
+              <StepLabel>{activeStep === 2 ? "Fertig!" : ""}</StepLabel>
+            </Step>
+          </Stepper>
+          {tracking.lastMessage && (
+            <div className={classes.message}>
+              <Typography variant="subtitle2">Letztes Update:</Typography>
+              <Typography className={classes.date} variant="subtitle1">
+                <i>{tracking.lastMessageDate?.toLocaleDateString() + " "}</i>
+                <i>
+                  {tracking.lastMessageDate?.toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </i>
+              </Typography>
+              <Typography>
+                <i>{tracking.lastMessage}</i>
+              </Typography>
+            </div>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary">
+            Schließen
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
