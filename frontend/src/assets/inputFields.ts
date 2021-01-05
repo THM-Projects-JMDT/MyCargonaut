73e0c2be64bd89512c78895e018abb5f24208a88
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2021-01-05 02:31:54 +0100

    added new InputFields

--- frontend/src/assets/inputFields.ts
+++ frontend/src/assets/inputFields.ts
@@ -1,4 +1,4 @@
-export const inputFields = [
+export const inputFieldsReg = [
   {
     label: "Vorname",
   },
@@ -33,6 +33,58 @@ export const inputFields = [
   },
   {
     label: "Passwort wiederholen",
+    inputProps: {
+      type: "password",
+      name: "password",
+    },
+  },
+];
+
+export const inputFieldsProfile = [
+  {
+    label: "Vorname",
+  },
+  {
+    label: "Nachname",
+  },
+  {
+    label: "Geburtstag",
+  },
+  {
+    label: "Username",
+    inputProps: {
+      autoComplete: "off",
+      name: "username",
+    },
+  },
+  {
+    label: "E-Mail",
+    inputProps: {
+      autoComplete: "off",
+      type: "email",
+      name: "email",
+    },
+  },
+];
+
+export const inputFieldsPassword = [
+  {
+    label: "Altes Passwort eingeben",
+    inputProps: {
+      type: "password",
+      name: "password",
+    },
+  },
+  {
+    label: "Passwort",
+    inputProps: {
+      autoComplete: "new-password",
+      type: "password",
+      name: "password",
+    },
+  },
+  {
+    label: "Neues Passwort wiederholen",
     inputProps: {
       type: "password",
       name: "password",
