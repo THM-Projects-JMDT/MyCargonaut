e429f5b349fe9d9e7a40378006df13fbb2426db6
Jannik-PC <jannik.lapp@mni.thm.de>
2021-01-07 15:27:14 +0100

    added user into to each module

--- backend/src/offer/offer.module.ts
+++ backend/src/offer/offer.module.ts
@@ -3,12 +3,17 @@ import { OfferService } from "./offer.service";
 import { OfferController } from "./offer.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Offer, OfferSchema } from "./offer.schema";
+import { User, UserSchema } from "../users/user.schema";
+import { UsersService } from "../users/users.service";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Offer.name, schema: OfferSchema }]),
+    MongooseModule.forFeature([
+      { name: Offer.name, schema: OfferSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
-  providers: [OfferService],
+  providers: [OfferService, UsersService],
   controllers: [OfferController],
   exports: [OfferService],
 })
