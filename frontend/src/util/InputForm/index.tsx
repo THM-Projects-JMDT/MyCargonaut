31fc1b5c4ab26ea4c1eee2d6bce2801eec232f10
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2021-01-08 13:00:28 +0100

    added new input types

--- frontend/src/util/InputForm/index.tsx
+++ frontend/src/util/InputForm/index.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { FormControl, TextField, TextFieldProps } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  FormControl,
+  MenuItem,
+  TextField,
+  TextFieldProps,
+} from "@material-ui/core";
 import { useStyles } from "./InputForm.style";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { de } from "date-fns/locale";
 
 export interface InputField {
   label: string;
   inputProps?: TextFieldProps;
+  type: string;
+  items?: string[];
+  required?: boolean;
 }
 
 export interface InputFormProps {
@@ -13,20 +24,65 @@ export interface InputFormProps {
 
 export const InputForm: React.FC<InputFormProps> = ({ inputFields }) => {
   const classes = useStyles();
+  const [date, setDate] = useState<Date | null>(new Date());
+
+  const renderInputField = (field: InputField, idx: number) => {
+    switch (true) {
+      case field.type === "text":
+        return (
+          <TextField
+            key={idx}
+            fullWidth
+            label={field.label}
+            variant="outlined"
+            className={classes.input}
+            required={field.required ?? true}
+            {...field.inputProps}
+          />
+        );
+      case field.type === "select":
+        return (
+          <TextField
+            select
+            className={classes.input}
+            label={field.label}
+            variant="outlined"
+            required={field.required}
+            onChange={() => {}}
+            {...field.inputProps}
+          >
+            {field.items?.map((item) => (
+              <MenuItem value={item}>{item}</MenuItem>
+            ))}
+          </TextField>
+        );
+      case field.type === "date":
+        return (
+          <MuiPickersUtilsProvider utils={DateFnsUtils} locale={de}>
+            <DatePicker
+              inputVariant="outlined"
+              required={field.required}
+              className={classes.input}
+              disablePast
+              disableToolbar
+              variant="inline"
+              format="dd.MM.yyy"
+              label={field.label}
+              onChange={(date) => {
+                setDate(date);
+              }}
+              value={date}
+            />
+          </MuiPickersUtilsProvider>
+        );
+      default:
+        break;
+    }
+  };
 
   return (
     <FormControl fullWidth>
-      {inputFields.map((field, idx) => (
-        <TextField
-          key={idx}
-          fullWidth
-          label={field.label}
-          variant="outlined"
-          className={classes.input}
-          required
-          {...field.inputProps}
-        />
-      ))}
+      {inputFields.map((field, idx) => renderInputField(field, idx))}
     </FormControl>
   );
 };
