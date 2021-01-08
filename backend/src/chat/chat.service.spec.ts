f271be7756ea59469567c5a7447d631e6b53d47c
Jannik-PC <jannik.lapp@mni.thm.de>
2021-01-08 20:03:14 +0100

    added chat tests

--- backend/src/chat/chat.service.spec.ts
+++ backend/src/chat/chat.service.spec.ts
@@ -2,7 +2,9 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { ChatService } from "./chat.service";
 import { ChatController } from "./chat.controller";
 import {
+  addOffer,
   closeInMongodConnection,
+  loginAndGetJWTToken,
   rootMongooseTestModule,
 } from "../testUtil/MongooseTestModule";
 import { MongooseModule } from "@nestjs/mongoose";
@@ -20,13 +22,13 @@ import { AuthController } from "../auth/auth.controller";
 import { AuthService } from "../auth/auth.service";
 import { LocalStrategy } from "../auth/strategies/local.strategy";
 import { JwtStrategy } from "../auth/strategies/jwt.strategy";
+import { OfferController } from "../offer/offer.controller";
 
 describe("ChatService", () => {
   let userService: UsersService;
   let service: ChatService;
   let controller: ChatController;
   let app: INestApplication;
-  let jwtToken: string;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -56,7 +58,7 @@ describe("ChatService", () => {
         LocalStrategy,
         JwtStrategy,
       ],
-      controllers: [ChatController, AuthController],
+      controllers: [ChatController, AuthController, OfferController],
     }).compile();
 
     userService = moduleRef.get<UsersService>(UsersService);
@@ -73,8 +75,52 @@ describe("ChatService", () => {
   it("should be defined", () => {
     expect(controller).toBeDefined();
   });
+
+  it(`add chat`, async () => {
+    const [localJwtToken, username] = await loginAndGetJWTToken(
+      userService,
+      app
+    );
+    let response = await addOffer(app, localJwtToken, true);
+    response = await addChat(app, localJwtToken, response.body._id, "Hi");
+    expect(response.body.content).toBe("Hi");
+  });
+
+  it(`get chat`, async () => {
+    const [localJwtToken, username] = await loginAndGetJWTToken(
+      userService,
+      app
+    );
+    const offer = await addOffer(app, localJwtToken, true);
+    await addChat(app, localJwtToken, offer.body._id, "Hi");
+    let response = await request(app.getHttpServer())
+      .get("/chat/" + offer.body._id)
+      .set("Authorization", `Bearer ${localJwtToken}`);
+    expect(response.body[0].content).toBe("Hi");
+    await addChat(app, localJwtToken, offer.body._id, "Test123");
+    response = await request(app.getHttpServer())
+      .get("/chat/" + offer.body._id)
+      .set("Authorization", `Bearer ${localJwtToken}`);
+    expect(response.body[0].content).toBe("Hi");
+    expect(response.body[1].content).toBe("Test123");
+  });
+
   afterAll(async () => {
     await closeInMongodConnection();
     await app.close();
   });
 });
+
+export const addChat = async (
+  app,
+  localJwtToken,
+  offerId: string,
+  content: string
+) => {
+  return request(app.getHttpServer())
+    .post("/chat/" + offerId)
+    .send({
+      content: content,
+    })
+    .set("Authorization", `Bearer ${localJwtToken}`);
+};
