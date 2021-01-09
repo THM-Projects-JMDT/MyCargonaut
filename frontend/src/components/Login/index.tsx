1d9ab657aef73cc8c82a2db8cb3aac7a23282147
Max Stephan <max.stephan@mni.thm.de>
2021-01-09 14:25:04 +0100

    frontend Login: implement login logic

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { createRef, useEffect } from "react";
 import { CustomCard } from "../../util/CustomCard";
 import { InputForm } from "../../util/InputForm";
 import { CenterCard } from "../../util/CenterCard";
 import { useHistory } from "react-router-dom";
 import { routes } from "../../routes";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../features/authSlice";
+import { RootState } from "../../features/rootReducer";
 
 export const Login = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const usernameRef = createRef<HTMLInputElement>();
+  const passwordRef = createRef<HTMLInputElement>();
+  const logedIn = useSelector((state: RootState) => state.auth.isLogedIn);
+
+  useEffect(() => {
+    if (logedIn) history.push(routes.home.path);
+  }, [logedIn, history]);
 
   const handleLogin = () => {
-    //TODO Login logic
-    history.push(routes.home.path);
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log(usernameRef.current);
+
+    if (!username || !password) return;
+
+    dispatch(login(username, password));
   };
 
   const handlePressEnter = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -24,6 +40,7 @@ export const Login = () => {
       label: "E-Mail/Username",
       type: "text",
       inputProps: {
+        inputRef: usernameRef,
         onKeyDown: handlePressEnter,
         autoComplete: "username",
         name: "username",
@@ -33,6 +50,7 @@ export const Login = () => {
       label: "Passwort",
       type: "text",
       inputProps: {
+        inputRef: passwordRef,
         onKeyDown: handlePressEnter,
         autoComplete: "current-password",
         type: "password",
