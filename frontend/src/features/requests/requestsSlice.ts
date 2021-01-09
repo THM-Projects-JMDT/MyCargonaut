89f2e743638b850e65b8ca50195bf5c801481cf4
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2021-01-09 13:18:46 +0100

    added Slices

---
+++ frontend/src/features/requests/requestsSlice.ts
@@ -0,0 +1,70 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Offer } from "../../../../backend/src/offer/offer";
+import {
+  addRequest,
+  getAllRequests,
+  getPersonalRequests,
+  OfferResponse,
+} from "../../api/offers";
+import { AppThunk } from "../store";
+
+export interface RequestsState {
+  allRequests: OfferResponse[];
+  personalRequests: OfferResponse[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: RequestsState = {
+  allRequests: [],
+  personalRequests: [],
+  isLoading: true,
+  error: null,
+};
+
+export const requests = createSlice({
+  name: "requests",
+  initialState,
+  reducers: {
+    getRequestsSucces(
+      state,
+      {
+        payload,
+      }: PayloadAction<{
+        allRequests: OfferResponse[];
+        personalRequests: OfferResponse[];
+      }>
+    ) {
+      state.allRequests = payload.allRequests;
+      state.personalRequests = payload.personalRequests;
+      state.isLoading = false;
+    },
+    getRequestsFailure(state, { payload }: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = payload;
+    },
+  },
+});
+
+export const { getRequestsSucces, getRequestsFailure } = requests.actions;
+
+export default requests.reducer;
+
+export const fetchRequests = (): AppThunk => async (dispatch) => {
+  try {
+    const allRequests = await getAllRequests();
+    const personalRequests = await getPersonalRequests();
+    dispatch(getRequestsSucces({ allRequests, personalRequests }));
+  } catch (e) {
+    dispatch(getRequestsFailure(e.toString()));
+  }
+};
+
+export const putRequest = (request: Offer): AppThunk => async (dispatch) => {
+  try {
+    await addRequest(request);
+    dispatch(fetchRequests());
+  } catch (e) {
+    dispatch(getRequestsFailure(e.toString()));
+  }
+};
---
+++ frontend/src/features/vehicles/vehiclesSlice.ts
@@ -0,0 +1,73 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Car } from "../../../../backend/src/car/car";
+import { addVehicle, getVehicles, removeVehicle } from "../../api/vehicles";
+import { AppThunk } from "../store";
+
+export interface VehicleResponse extends Car {
+  _id: string;
+}
+
+export interface VehiclesState {
+  vehicles: Car[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: VehiclesState = {
+  vehicles: [],
+  isLoading: true,
+  error: null,
+};
+
+export const vehicles = createSlice({
+  name: "vehicles",
+  initialState,
+  reducers: {
+    getVehiclesSucces(state, { payload }: PayloadAction<Car[]>) {
+      state.vehicles = payload;
+      state.isLoading = false;
+    },
+    putVehicleSuccess(state, { payload }: PayloadAction<Car>) {
+      state.vehicles.push(payload);
+    },
+    getVehiclesFailure(state, { payload }: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = payload;
+    },
+  },
+});
+
+export const {
+  getVehiclesSucces,
+  getVehiclesFailure,
+  putVehicleSuccess,
+} = vehicles.actions;
+
+export default vehicles.reducer;
+
+export const fetchVehicles = (): AppThunk => async (dispatch) => {
+  try {
+    const vehicles = await getVehicles();
+    dispatch(getVehiclesSucces(vehicles));
+  } catch (e) {
+    dispatch(getVehiclesFailure(e.toString()));
+  }
+};
+
+export const putVehicle = (vehicle: Car): AppThunk => async (dispatch) => {
+  try {
+    const newVehicle = await addVehicle(vehicle);
+    dispatch(putVehicleSuccess(newVehicle));
+  } catch (e) {
+    dispatch(getVehiclesFailure(e.toString()));
+  }
+};
+
+export const deleteVehicle = (id: number): AppThunk => async (dispatch) => {
+  try {
+    await removeVehicle(id);
+    dispatch(fetchVehicles());
+  } catch (e) {
+    dispatch(getVehiclesFailure(e.toString()));
+  }
+};
