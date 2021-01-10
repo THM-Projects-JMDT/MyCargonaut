31a206d17c1326f1225426f760267c8208b0ac88
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2021-01-10 14:24:26 +0100

    added fetch

--- frontend/src/api/offers.ts
+++ frontend/src/api/offers.ts
@@ -8,25 +8,81 @@ export interface OfferResponse extends Offer {
 }
 
 export const getAllOffers = async (): Promise<OfferResponse[]> => {
-  return new Promise<OfferResponse[]>((res, rej) => {});
+  return fetch("/api/v1/offer?forOffer=true", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse[]>;
 };
 
 export const getAllRequests = async (): Promise<OfferResponse[]> => {
-  return new Promise<OfferResponse[]>((res, rej) => {});
+  return fetch("/api/v1/offer", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse[]>;
 };
 
 export const getPersonalOffers = async (): Promise<OfferResponse[]> => {
-  return new Promise<OfferResponse[]>((res, rej) => {});
+  return fetch("/api/v1/offer?forOffer=true&forPrivate=true", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse[]>;
 };
 
 export const getPersonalRequests = async (): Promise<OfferResponse[]> => {
-  return new Promise<OfferResponse[]>((res, rej) => {});
+  return fetch("/api/v1/offer?forPrivate=true", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse[]>;
 };
 
 export const addOffer = async (offer: Offer): Promise<OfferResponse> => {
-  return new Promise<OfferResponse>((res, rej) => {});
+  return fetch("/api/v1/addOffer", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+    body: JSON.stringify({ ...offer, isOffer: true }),
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse>;
 };
 
 export const addRequest = async (request: Offer): Promise<OfferResponse> => {
-  return new Promise<OfferResponse>((res, rej) => {});
+  return fetch("/api/v1/addOffer", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+    body: JSON.stringify(request),
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse>;
+};
+
+export const bookOffer = async (id: number): Promise<OfferResponse> => {
+  return fetch(`/api/v1/bookOffer/${id}`, {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<OfferResponse>;
 };
--- frontend/src/api/vehicles.ts
+++ frontend/src/api/vehicles.ts
@@ -1,13 +1,40 @@
 import { Car } from "../../../backend/src/car/car";
 
 export const getVehicles = async (): Promise<Car[]> => {
-  return new Promise<Car[]>((res, rej) => {});
+  return fetch("/api/v1/car", {
+    headers: {
+      "content-type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<Car[]>;
 };
 
 export const addVehicle = async (vehicle: Car): Promise<Car> => {
-  return new Promise<Car>((res, rej) => {});
+  return fetch("/api/v1/car", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+    body: JSON.stringify(vehicle),
+  }).then((res) => {
+    return res.json();
+  }) as Promise<Car>;
 };
 
 export const removeVehicle = async (id: number): Promise<void> => {
-  return new Promise<void>((res, rej) => {});
+  return fetch(`/api/v1/car/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmFlN2M4ZTY2YjE1MWVkMDE4ZjQ2YyIsImlhdCI6MTYxMDI4MTYzMSwiZXhwIjoxNjEwODAwMDMxfQ.pvD8SldGD2_6v1vOuCI_3Fn1AGmlAelnKBn8KQGbxOs",
+    },
+  }).then((res) => {
+    return res.json();
+  }) as Promise<void>;
 };
