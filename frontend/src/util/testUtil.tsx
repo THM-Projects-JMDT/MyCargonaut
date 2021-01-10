540af9237cd42af29f26f96ae1987fbfea044a0a
Max Stephan <max.stephan@mni.thm.de>
2021-01-10 22:27:26 +0100

    frontend testUtil: added setInputValue

--- frontend/src/util/testUtil.tsx
+++ frontend/src/util/testUtil.tsx
@@ -1,7 +1,13 @@
 // Inspired by https://dev.to/slawomirkolodziej/guide-testing-redux-connected-components-with-react-testing-library-and-jest-43j7
 
 import React from "react";
-import { render, RenderOptions } from "@testing-library/react";
+import {
+  fireEvent,
+  Matcher,
+  render,
+  RenderOptions,
+  SelectorMatcherOptions,
+} from "@testing-library/react";
 import { createStore } from "redux";
 import { Provider, RootStateOrAny } from "react-redux";
 import reducer from "../features/rootReducer";
@@ -30,4 +36,18 @@ export const renderWithState = (
   );
 
   return render(ui, { wrapper: Wrapper, ...renderOptions });
+};
+
+export const setInputValue = (
+  getByTestId: (
+    text: Matcher,
+    options?: SelectorMatcherOptions | undefined,
+    waitForElementOptions?: unknown
+  ) => HTMLElement,
+  label: string,
+  value: string
+) => {
+  // Get Input Element from Material ui TextField (TODO improve?)
+  const input = getByTestId(label).children[1].children[0];
+  fireEvent.change(input, { target: { value: value } });
 };
