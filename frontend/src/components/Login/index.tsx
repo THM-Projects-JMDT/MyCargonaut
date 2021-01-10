353356f0f23ccbe7a58344df926500c81c1f13de
Max Stephan <max.stephan@mni.thm.de>
2021-01-10 22:27:54 +0100

    frontend Login: added ids

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -36,6 +36,7 @@ export const Login = () => {
         onKeyDown: handlePressEnter,
         autoComplete: "username",
         name: "username",
+        id: "username",
       },
     },
     {
@@ -47,6 +48,7 @@ export const Login = () => {
         autoComplete: "current-password",
         type: "password",
         name: "password",
+        id: "password",
       },
     },
   ];
