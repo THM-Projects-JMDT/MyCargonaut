875b544f3bb0346a27b942e53d660afaccc7979e
TimonPllkrn <timon.pellekoorne@mni.thm.de>
2021-01-12 16:52:51 +0100

    add Dashboard

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,9 +1,37 @@
+import { List, ListItem, Typography } from "@material-ui/core";
 import React from "react";
+import { Offer } from "../Offer";
+import { OfferDummy } from "../Offer/OfferDummy";
 
 export const Dashboard = () => {
+  const [displayList, setDisplayList] = React.useState<any[] | undefined>([]);
+  const displayName = "offers";
+
+  const predicate = (o: any) => {
+    return (
+      (!filter.from || o.offer.from.toLowerCase() === filter.from) &&
+      (!filter.to || o.offer.to.toLowerCase() === filter.to) &&
+      (filter.service === "both" || o.offer.service === filter.service)
+    );
+  };
+
   return (
     <div>
-      <h1>My Cargonaut</h1>
+      <Typography variant="h3">Meine Anfragen</Typography>
+      <List>
+        <OfferDummy show={"offers"} />
+        {displayList?.filter(predicate).map((o: any, idx: number) => (
+          <ListItem key={idx}>
+            <Offer
+              offer={o.offer}
+              customer={o.customer}
+              provider={o.provider}
+              loggedInUserId={loggedInUserId}
+            />
+          </ListItem>
+        ))}
+      </List>
+      <Typography variant="h3">Meine Angebote</Typography>
     </div>
   );
 };
