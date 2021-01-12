29117f8370fff8a62715903a85a984e0594f1dea
Max Stephan <max.stephan@mni.thm.de>
2021-01-12 17:24:26 +0100

    backend user controller: added profile upload

--- backend/src/users/user.controller.ts
+++ backend/src/users/user.controller.ts
@@ -1,15 +1,24 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Post,
   Put,
   Request,
+  Res,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
+import { editFileName, profileImageFileFilter } from "./profileUploadUtils";
 import { UsersService } from "./users.service";
+import { diskStorage } from "multer";
 
 @Controller("user")
 @UseGuards(JwtAuthGuard)
@@ -43,5 +52,29 @@ export class UserController {
   @Get()
   async getUser(@Request() req) {
     return this.userService.findOneById(req.user.id);
+  }
+
+  @Post("profile/upload")
+  @UseInterceptors(
+    FileInterceptor("file", {
+      storage: diskStorage({
+        destination: "./profile",
+        filename: editFileName,
+      }),
+      limits: { fileSize: 1000000 },
+      fileFilter: profileImageFileFilter,
+    })
+  )
+  uploadFile(@UploadedFile() file) {
+    console.log(file);
+  }
+
+  @Get("profile/:uid")
+  seeUploadedFile(@Param("uid") image, @Res() res) {
+    try {
+      return res.sendFile(image, { root: "./profile" });
+    } catch {
+      throw new HttpException("File not found", HttpStatus.BAD_REQUEST);
+    }
   }
 }
